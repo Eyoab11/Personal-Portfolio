3db9c81814b46586913b77890ddef029a638902e
Eyoab11 <134016127+Eyoab11@users.noreply.github.com>
2025-02-11 01:04:33 +0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,8 @@ import About from '../components/About';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
 import NavBar from '../components/NavBar';
+import Experience from '../components/Experience'
+import Certificates from '../components/Certificates';
 const HomePage = () => {
   return (
     <>
@@ -15,6 +17,13 @@ const HomePage = () => {
       <div id="about" className="mb-12 mt-20"> {/* Margin bottom for About component */}
         <About />
       </div>
+      <div id="Experience" className="mb-12 mt-20"> {/* Margin bottom for Experience component */}
+        <Experience />
+      </div>
+      <div id="certificates" className="mb-12 mt-20"> {/* Margin bottom for Experience component */}
+        <Certificates />
+      </div>
+
       <div  id="projects" className="mb-12 mt-10"> {/* Margin bottom for Projects component */}
         <Projects />
       </div>
